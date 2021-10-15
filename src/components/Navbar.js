f114d5ddaadb8b13c07b85c7b3017a280bdbe5c8
Epicrafter <68658005+OscarBrehier@users.noreply.github.com>
2021-10-15 14:58:56 +0300

    🔥 | First Commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { data } from '../data/user-data';
+import '../styles/Navbar.css';
+
+export default function Navbar() {
+
+    return (
+
+        <div className='container flex justify-between relative'>
+            <div className='navLeft absolute left bottom'>
+
+            </div>
+            <div className='navRight absolute right bottom'>
+
+            </div>
+        </div>
+
+    )
+
+};
---
+++ src/data/user-data.js
@@ -0,0 +1,6 @@
+export const data = {
+    name: 'Oscar Bréhier',
+    denomination: 'Full Stack Web Developer',
+    description: 'A 16 year old full stack web developer',
+    pages: ['work', 'about', 'contact']
+}
