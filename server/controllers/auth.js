68e12ff7bfe8d47c3aba36ee85bfbb2a537e8b0b
Epicrafter <68658005+OscarBrehier@users.noreply.github.com>
2021-10-18 15:42:01 +0300

    🎨 | Some minor changes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,13 +1,5 @@
-import config from "../config.js";
-
 export const login = async (req, res) => {
 
-    const { username, password } = req.body;
 
-    if(username === config.username && password === config.password) {
-
-        console.log('Logged In');
-
-    }
 
 }
--- server/routes/router.js
+++ server/routes/router.js
@@ -3,13 +3,5 @@ const router = express.Router();
 import { login } from "../controllers/auth.js";
 
 router.get('/login', login);
-router.get('/status', (req, res) => {
-
-    res.json({
-        message: 'Success',
-        status: 200
-    });
-
-});
 
 export default router;
