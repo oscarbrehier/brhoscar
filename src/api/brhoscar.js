f1d3e17a759c258e3a7fedb75cc56feb0b8bdf45
Epicrafter <68658005+OscarBrehier@users.noreply.github.com>
2021-10-21 19:07:19 +0300

    🧨 | Minor changes

--- src/api/brhoscar.js
+++ src/api/brhoscar.js
@@ -2,20 +2,12 @@ import axios from "axios";
 
 const url = 'https://brhoscar-api.herokuapp.com/post';
 
-export const postPost = (body) => axios({
-    url: url,
-    method: 'POST',
-    headers: config.brhoscar.token || process.env.token,
-    data: body
-});
-
-
 export const getPosts = async () => {
 
     const response = await fetch(url, {
         method: 'GET',
         headers: {
-            token: config.brhoscar.token
+            token: process.env.token
         }
     });
 
