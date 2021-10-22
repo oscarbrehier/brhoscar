c80849a8f9d24cab3095116d930cbb91cb2eed1b
Epicrafter <68658005+OscarBrehier@users.noreply.github.com>
2021-10-22 13:46:13 +0300

    🛍 | Project layout change

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -5,13 +5,13 @@ export default function Project(title, description, languages, url) {
 
     return (
 
-        <div className='wpContainer flex'>
-            {/*<div className='wpContainer--title capitalize '>{ title }</div>*/}
-            {/*<div className='wpContainer--description lowercase'>{ description }</div>*/}
-            <p>
-                <a href={url} target='blank' className='wpContainer--title capitalize'>{title}</a>
-                <span className='wpContainer--description lowercase'>{description}</span>
-            </p>
+        <div className='wpContainer'>
+            <div className='flex'>
+                <p>
+                    <a href={url} target='blank'><span className='wpContainer--title capitalize underline'>{title}</span></a>
+                    <span className='wpContainer--description lowercase'>{description}</span>
+                </p>
+            </div>
         </div>
 
     )
