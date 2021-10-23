18fabd9977a8d1419af6a99ab8c4bea1164bfea8
Epicrafter <68658005+OscarBrehier@users.noreply.github.com>
2021-10-23 17:54:29 +0300

    🎢 | Added about section

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import './style.css';
+import './about.css';
 import './phone-view.css';
 
 export default function Homepage() {
@@ -19,15 +20,26 @@ export default function Homepage() {
                     </div>
                     <div className='hContainer__wrapper--p'>
                         <p className='tBlack'>
-                            I'm a self taught front-end web developer
+                            I'm a self taught front-end developer
                             <br/>
                             and designer
                         </p>
                     </div>
                 </div>
             </div>
-            <div className='h-100'>
-
+            <div className='hAbout h-auto'>
+                <div className='hAbout__wrapper h-inherit'>
+                    <div>
+                        <p className='hAbout--title w700'>hey.</p>
+                        <p className='hAbout--subtitle'>Hello, I am Oscar, a young 16 year-old front-end developer and designer .</p>
+                        <p className='hAbout--paragraph'>
+                            I have 3+ years of experience working on various projects and still looking forward.
+                            I spend most of my time coding scripts and building websites.
+                            Through this, I have learned self-taught web development, graphic design and some video editing.
+                            Current coding languages are JavaScript, HTML, CSS and Python.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
 
