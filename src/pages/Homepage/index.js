8410806d6272feaad1549d7b2a6c502cc9903cde
Epicrafter <68658005+OscarBrehier@users.noreply.github.com>
2021-10-23 18:01:54 +0300

    🎞 | Small error

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -33,7 +33,7 @@ export default function Homepage() {
                         <p className='hAbout--title w700'>hey.</p>
                         <p className='hAbout--subtitle'>Hello, I am Oscar, a young 16 year-old front-end developer and designer .</p>
                         <p className='hAbout--paragraph'>
-                            I have 3+ years of experience working on various projects and still looking forward.
+                            I have 3+ years of experience working on various projects and still looking forward to learning more.
                             I spend most of my time coding scripts and building websites.
                             Through this, I have learned self-taught web development, graphic design and some video editing.
                             Current coding languages are JavaScript, HTML, CSS and Python.
